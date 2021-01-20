f2efda14de947a039df51a1b8ce849fce09d24bd
Sal Gamez <gamezsalvador@gmail.com>
2021-01-19 22:22:10 -0700

    added more app.get routes to server.js file

--- server.js
+++ server.js
@@ -31,10 +31,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
