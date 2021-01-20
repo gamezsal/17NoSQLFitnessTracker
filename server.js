4039feed9a79ab9d8e18e66d88fcad45c7b27ae7
Sal Gamez <gamezsalvador@gmail.com>
2021-01-19 17:48:54 -0700

    added readme file

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true,
   useFindAndModify: false
