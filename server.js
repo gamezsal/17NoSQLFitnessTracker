2c964cb5806d0a2986a49d28455f9d450092151c
Sal Gamez <gamezsalvador@gmail.com>
2021-01-20 22:11:40 -0700

    fixed connection for mongoose to reflect workout

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/fitnessapp',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
